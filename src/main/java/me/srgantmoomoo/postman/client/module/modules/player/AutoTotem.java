c6da7aab146d9ea7658b8cee54bc01643402b2e5
moomooooo <goldenrighty@gmail.com>
2021-01-19 21:04:59 -0500

    optimizing auto totem.

--- src/main/java/me/srgantmoomoo/postman/client/module/modules/player/AutoTotem.java
+++ src/main/java/me/srgantmoomoo/postman/client/module/modules/player/AutoTotem.java
@@ -4,11 +4,10 @@ import org.lwjgl.input.Keyboard;
 
 import me.srgantmoomoo.postman.client.module.Category;
 import me.srgantmoomoo.postman.client.module.Module;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.ClickType;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
 
 public class AutoTotem extends Module {
 	
@@ -16,60 +15,60 @@ public class AutoTotem extends Module {
 		super ("autoTotem", "automatically places totem in ur offhand", Keyboard.KEY_NONE, Category.PLAYER);
 	}
 	
-	int totems;
-	boolean moving = false;
-	boolean returnI = false;
-	
-	private Minecraft mc = Minecraft.getMinecraft();
-	
-	@Override
+	private boolean switching = false;
+    private int last_slot;
+
+    @Override
     public void onUpdate() {
-			
-		if (mc.currentScreen instanceof GuiContainer) return;
-		if (returnI){
-			int t = -1;
-			for (int i = 0; i < 45; i++)
-				if (mc.player.inventory.getStackInSlot(i).isEmpty()){
-					t = i;
-					break;
-				}
-			
-			if (t == -1) return;
-			mc.playerController.windowClick(0, t < 9 ? t + 36 : t, 0, ClickType.PICKUP, mc.player);
-			returnI = false;
-		}
-		totems = mc.player.inventory.mainInventory.stream().filter(itemStack -> itemStack.getItem() == Items.TOTEM_OF_UNDYING).mapToInt(ItemStack::getCount).sum();
-		if (mc.player.getHeldItemOffhand().getItem() == Items.TOTEM_OF_UNDYING) totems++;
-		else{
-			if (!mc.player.getHeldItemOffhand().isEmpty()) return;
-			if (moving){
-				mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
-				moving = false;
-				if (!mc.player.inventory.getItemStack().isEmpty()) returnI = true;
-				return;
-			}
-			if (mc.player.inventory.getItemStack().isEmpty()){
-				if (totems == 0) return;
-				int t = -1;
-				for (int i = 0; i < 45; i++)
-					if (mc.player.inventory.getStackInSlot(i).getItem() == Items.TOTEM_OF_UNDYING){
-						t = i;
-						break;
-					}
-				if (t == -1) return; 
-				mc.playerController.windowClick(0, t < 9 ? t + 36 : t, 0, ClickType.PICKUP, mc.player);
-				moving = true;
-			}else {
-					int t = -1;
-					for (int i = 0; i < 45; i++)
-						if (mc.player.inventory.getStackInSlot(i).isEmpty()){
-							t = i;
-							break;
-						}
-					if (t == -1) return;
-					mc.playerController.windowClick(0, t < 9 ? t + 36 : t, 0, ClickType.PICKUP, mc.player);
-					}
-				}
-			}
+
+        if (mc.currentScreen == null || mc.currentScreen instanceof GuiInventory) {
+
+            if (switching) {
+            	swapTotem(last_slot, 2);
+                return;
+            }
+
+            if (mc.player.getHeldItemOffhand().getItem() == Items.AIR) {
+            	swapTotem(getTotem(), 0);
+            }
+
+        }
+
+    }
+
+    private int getTotem() {
+        if (Items.TOTEM_OF_UNDYING == mc.player.getHeldItemOffhand().getItem()) return -1;
+        for(int i = 36; i >= 0; i--) {
+            final Item item = mc.player.inventory.getStackInSlot(i).getItem();
+            if(item == Items.TOTEM_OF_UNDYING) {
+                if (i < 9) {
+                    return -1;
+                }
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void swapTotem(int slot, int step) {
+        if (slot == -1) return;
+        if (step == 0) {
+            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
+            mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
+            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
+        }
+        if (step == 1) {
+            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
+            switching = true;
+            last_slot = slot;
+        }
+        if (step == 2) {
+            mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
+            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
+            switching = false;
+        }
+
+        mc.playerController.updateController();
+    }
 }
 
