c1d9ddb8334667c3547873586e716bf80e737006
moomooooo <goldenrighty@gmail.com>
2021-01-25 22:14:34 -0500

    start of command stuff lul.

---
+++ src/main/java/me/srgantmoomoo/postman/client/command/Command.java
@@ -0,0 +1,54 @@
+package me.srgantmoomoo.postman.client.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import scala.actors.threadpool.Arrays;
+
+public class Command {
+	public String name, description, syntax;
+	public List<String> aliases = new ArrayList<String>();
+	
+	public Command(String name, String description, String syntax, String... aliases) {
+		this.name = name;
+		this.description = description;
+		this.syntax = syntax;
+		this.aliases = Arrays.asList(aliases);
+	}
+	
+	public void onCommand(String[] args, String command) {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSyntax() {
+		return syntax;
+	}
+
+	public void setSyntax(String syntax) {
+		this.syntax = syntax;
+	}
+
+	public List<String> getAliases() {
+		return aliases;
+	}
+
+	public void setAliases(List<String> aliases) {
+		this.aliases = aliases;
+	}
+}
---
+++ src/main/java/me/srgantmoomoo/postman/client/command/CommandManager.java
@@ -0,0 +1,5 @@
+package me.srgantmoomoo.postman.client.command;
+
+public class CommandManager {
+
+}
