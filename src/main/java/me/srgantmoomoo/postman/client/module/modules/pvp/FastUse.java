f3058aeab50c57f3a9579bc80bc8ff8ad82be797
scaffpine <mikekec@gmail.com>
2021-03-06 01:00:04 +1300

    better fastuse

--- src/main/java/me/srgantmoomoo/postman/client/module/modules/pvp/FastUse.java
+++ src/main/java/me/srgantmoomoo/postman/client/module/modules/pvp/FastUse.java
@@ -13,29 +13,45 @@ import net.minecraft.util.math.BlockPos;
 
 public class FastUse extends Module {
 	
-	public boolean plswork;
 	public BooleanSetting xpBottle = new BooleanSetting("xpBottle", this, true);
 	public BooleanSetting bow = new BooleanSetting("bow", this, true);
-	
+	public BooleanSetting crystal = new BooleanSetting("crystal", this, true);
+	public BooleanSetting destroy = new BooleanSetting("destroy", this, true);
+	public BooleanSetting all = new BooleanSetting("all", this, true);
+
+
 	public FastUse() {
-		super ("fastUse", "lol bow and xp bottle go brrrrrrrr.", Keyboard.KEY_NONE, Category.PVP);
-		this.addSettings(xpBottle, bow);
+		super ("fastUse", "Removes usage and break delay from certain items", Keyboard.KEY_NONE, Category.PVP);
+		this.addSettings(xpBottle, bow, crystal, destroy, all);
 	}
 	private Minecraft mc = Minecraft.getMinecraft();
 	
 	@Override
 	public void onUpdate() {
-		
-		if (bow.isEnabled() && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= 3 && (mc.player.getHeldItemMainhand().getItem() == Items.BOW || mc.player.getHeldItemOffhand().getItem() == Items.BOW)) {
-			mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, mc.player.getHorizontalFacing()));
-			mc.player.connection.sendPacket(new CPacketPlayerTryUseItem(mc.player.getActiveHand()));
-			mc.player.stopActiveHand();
+		if (mc.player != null) {
+			if (bow.isEnabled() && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= 3 && (mc.player.getHeldItemMainhand().getItem() == Items.BOW || mc.player.getHeldItemOffhand().getItem() == Items.BOW)) {
+				mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, mc.player.getHorizontalFacing()));
+				mc.player.connection.sendPacket(new CPacketPlayerTryUseItem(mc.player.getActiveHand()));
+				mc.player.stopActiveHand();
 			}
-		
-		if (xpBottle.isEnabled() && mc.player != null && (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getHeldItemOffhand().getItem() == Items.EXPERIENCE_BOTTLE)) {
-				mc.rightClickDelayTimer = 0; 
+			if (xpBottle.isEnabled() && (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getHeldItemOffhand().getItem() == Items.EXPERIENCE_BOTTLE)) {
+				mc.rightClickDelayTimer = 0;
+			}
+			if (crystal.isEnabled() && (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL || mc.player.getHeldItemOffhand().getItem() == Items.END_CRYSTAL)) {
+				mc.rightClickDelayTimer = 0;
+			}
+			if (destroy.isEnabled()) {
+				mc.playerController.blockHitDelay = 0;
+			}
+			if (all.isEnabled()) {
+				mc.rightClickDelayTimer = 0;
+			}
 		}
-		}
-	
+	}
+	@Override
+	public void onDisable() {
+		mc.rightClickDelayTimer = 4;
+		mc.playerController.blockHitDelay = 5;
+	}
 }
 
