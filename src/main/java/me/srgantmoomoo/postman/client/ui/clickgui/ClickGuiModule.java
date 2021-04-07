c4a0d9bcfa2473d11f744322709549bbfd4cb08f
moomooooo <69589624+moomooooo@users.noreply.github.com>
2021-04-07 12:04:06 -0400

    clickgui color variable

--- src/main/java/me/srgantmoomoo/postman/client/ui/clickgui/ClickGuiModule.java
+++ src/main/java/me/srgantmoomoo/postman/client/ui/clickgui/ClickGuiModule.java
@@ -27,11 +27,11 @@ public class ClickGuiModule extends Module {
 	public NumberSetting scrolls = new NumberSetting("scrollSpeed", this, 10, 0, 100, 1);
 	public ModeSetting scrollMode = new ModeSetting("scroll", this, "container", "container", "screen");
 	public ModeSetting description = new ModeSetting("description", this, "mouse", "mouse", "fixed");
-	public ColorSetting enabledColor = new ColorSetting("enabledColor", this, new JColor(121, 193, 255, 255)); //(0, 157, 255, 255));
+	public ColorSetting enabledColor = new ColorSetting("enabledColor", this, new JColor(Reference.POSTMAN_COLOR, 255)); //(0, 157, 255, 255));
 	public ColorSetting backgroundColor = new ColorSetting("bgColor", this, new JColor(0, 0, 0, 255)); //(0, 121, 194, 255));
 	public ColorSetting settingBackgroundColor = new ColorSetting("settinBgColor", this, new JColor(0, 0, 0, 255));
 	public ColorSetting outlineColor = new ColorSetting("settingsHighlight", this, new JColor(255, 255, 255, 255));
-	public ColorSetting fontColor = new ColorSetting("categoryColor", this, new JColor(121, 193, 255, 255)); 
+	public ColorSetting fontColor = new ColorSetting("categoryColor", this, new JColor(Reference.POSTMAN_COLOR, 255)); 
 	public NumberSetting opacity = new NumberSetting("opacity", this, 255, 0, 255, 5);
 	
 	public BooleanSetting thinGui = new BooleanSetting("thinGui", this, false);
