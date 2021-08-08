0b3f97455d65a43e0280e62086e39dbf99c691f6
cattyngmd <cvenby@gmail.com>
2021-08-08 19:42:17 +0300

    $$$

--- src/main/java/me/srgantmoomoo/postman/client/command/commands/Help.java
+++ src/main/java/me/srgantmoomoo/postman/client/command/commands/Help.java
@@ -2,6 +2,7 @@ package me.srgantmoomoo.postman.client.command.commands;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
 
+import me.srgantmoomoo.Main;
 import me.srgantmoomoo.Reference;
 import me.srgantmoomoo.postman.client.command.Command;
 import me.srgantmoomoo.postman.client.command.CommandManager;
@@ -9,16 +10,6 @@ import me.srgantmoomoo.postman.client.module.ModuleManager;
 import net.minecraft.util.text.TextFormatting;
 
 public class Help extends Command {
-	public static Prefix prefix;
-	public static Toggle toggle;
-	public static Bind bind;
-	public static Baritone baritone;
-	public static Friend friend;
-	public static AutoCope autoCope;
-	public static Protester protester;
-	public static MobOwner mobOwner;
-	public static Clip clip;
-	public static Vanish vanish;
 	
 	public Help() {
 		super("help", "helps lol.", "help", "h");
@@ -31,37 +22,18 @@ public class Help extends Command {
     TextFormatting BOLD = TextFormatting.BOLD;
     TextFormatting ITALIC = TextFormatting.ITALIC;
     TextFormatting RED = TextFormatting.RED;
-	
+
 	@Override
 	public void onCommand(String[] args, String command) {
-		String PREFIX = CommandManager.prefix;
-		
-		prefix = new Prefix();
-		toggle = new Toggle();
-		bind = new Bind();
-		baritone = new Baritone();
-		friend = new Friend();
-		autoCope = new AutoCope();
-		protester = new Protester();
-		mobOwner = new MobOwner();
-		clip = new Clip();
-		vanish = new Vanish();
 		
 		ModuleManager.addChatMessage(ChatFormatting.GREEN + "-------------------");
 		
 		ModuleManager.addChatMessage(ChatFormatting.BOLD + Reference.NAME + " " + Reference.VERSION + "!");
 
-		helpMessage(prefix.name, prefix.description, prefix.syntax);
-		helpMessage(toggle.name, toggle.description, toggle.syntax);
-		helpMessage(bind.name, bind.description, bind.syntax);
-		helpMessage(baritone.name, baritone.description, baritone.syntax);
-		helpMessage(friend.name, friend.description, friend.syntax);
-		helpMessage(autoCope.name, autoCope.description, autoCope.syntax);
-		helpMessage(protester.name, protester.description, protester.syntax);
-		helpMessage(mobOwner.name, mobOwner.description, mobOwner.syntax);
-		helpMessage(clip.name, clip.description, clip.syntax);
-		helpMessage(vanish.name, vanish.description, vanish.syntax);
-		
+		Main.commandManager.commands.forEach(c -> {
+			helpMessage(c.name, c.description, c.syntax);
+		});
+
 		ModuleManager.addChatMessage(ChatFormatting.GREEN + "-------------------");
 
 	}
