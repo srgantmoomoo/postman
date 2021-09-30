e6c484ff1b7725cca790e642fb7824913c10e4ea
FlufferyAutism <74445505+FlufferyAutism@users.noreply.github.com>
2021-09-30 09:24:51 -0400

    Update Reference.java

--- src/main/java/me/srgantmoomoo/Reference.java
+++ src/main/java/me/srgantmoomoo/Reference.java
@@ -2,10 +2,12 @@ package me.srgantmoomoo;
 
 import me.srgantmoomoo.postman.api.util.render.JColor;
 
+//changed from pst to postman for easier debugging 
+
 public class Reference {
-	public static final String MOD_ID = "pst";
+	public static final String MOD_ID = "postman";
 	public static final String NAME = "postman";
-	public static final String VERSION = "2.110";
+	public static final String VERSION = "2.111";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "me.srgantmoomoo.postman.api.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "me.srgantmoomoo.postman.api.proxy.CommonProxy";
