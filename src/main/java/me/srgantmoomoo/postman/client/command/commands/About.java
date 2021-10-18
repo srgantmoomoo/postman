e1864d5a448cd98ebd9ec161819471f6def06ac6
FlufferyAutism <74445505+FlufferyAutism@users.noreply.github.com>
2021-10-18 00:31:04 -0400

    Add files via upload

---
+++ src/main/java/me/srgantmoomoo/postman/client/command/commands/About.java
@@ -0,0 +1,16 @@
+package me.srgantmoomoo.postman.client.command.commands;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+
+import me.srgantmoomoo.postman.client.command.Command;
+import me.srgantmoomoo.postman.client.command.CommandManager;
+import me.srgantmoomoo.postman.client.module.ModuleManager;
+
+public class About extends Command {
+	
+    public About() {
+		super("about", "tells you more about the client, "abt");
+	}
+// add more later, not really important atm	
+	}
+}
