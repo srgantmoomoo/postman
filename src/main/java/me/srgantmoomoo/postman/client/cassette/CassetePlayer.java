5f5819d26e254edf372fa5d97e1689164fe9e677
Fluffery <74445505+FlufferyGR@users.noreply.github.com>
2021-10-18 12:27:00 -0400

    Update CassetePlayer.java

--- src/main/java/me/srgantmoomoo/postman/client/cassette/CassetePlayer.java
+++ src/main/java/me/srgantmoomoo/postman/client/cassette/CassetePlayer.java
@@ -2,4 +2,6 @@ package me.srgantmoomoo.postman.client.cassette;
 
 public class CassetePlayer {
 // this is going to be a pain in the ass
+// still debating on what to use 
+// most likely spotify
 }
