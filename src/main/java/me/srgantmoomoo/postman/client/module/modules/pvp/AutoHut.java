636594d1fe1f25138f48774cd091bb7a1fbe87c2
FlufferyΕΛ <74445505+FlufferyGR@users.noreply.github.com>
2021-10-22 15:21:30 -0400

    Update AutoHut.java

--- src/main/java/me/srgantmoomoo/postman/client/module/modules/pvp/AutoHut.java
+++ src/main/java/me/srgantmoomoo/postman/client/module/modules/pvp/AutoHut.java
@@ -10,7 +10,43 @@ public class AutoHut extends Module {
 	public AutoHut() {
 		super ("autoHut", "automatically builds hut for u.", Keyboard.KEY_NONE, Category.PVP);
 
-		//coded but its throwing a shit ton of errors, i will fix it later
+		//ending part i lost the code for the rest of it idk why
+					}
+					
+		            mc.playerController.processRightClickBlock(mc.player, mc.world, neighbor, side2, hitVec, EnumHand.MAIN_HAND);
+					mc.player.swingArm(EnumHand.MAIN_HAND);
+					
+		            return true;
+				}
+				
+				return false;
+				
+		    }
+
+			private int find_in_hotbar() {
+
+		        for (int i = 0; i < 9; ++i) {
+
+		            final ItemStack stack = mc.player.inventory.getStackInSlot(i);
+
+		            if (stack != ItemStack.EMPTY && stack.getItem() instanceof ItemBlock) {
+
+		                final Block block = ((ItemBlock) stack.getItem()).getBlock();
+
+		                if (block instanceof BlockEnderChest)
+		                    return i;
+		                
+		                else if (block instanceof BlockObsidian)
+		                    return i;
+		                
+		            }
+		        }
+		        return -1;
+		    }
+
+
+		}
+		
 	}
 
 }
