217d3002c966083dc1abb8693f689aa80d432f7d
FlufferyGR <74445505+FlufferyGR@users.noreply.github.com>
2022-01-19 09:36:07 -0500

    fix later cus laptop moment xd

---
+++ src/main/java/me/srgantmoomoo/postman/client/module/modules/pvp/fastbow.java
@@ -0,0 +1,20 @@
+//doing this from my school chromebook ill fix this in intellij later
+//absolutely stolen btw 
+import net.minecraft.item.ItemBow;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
+import net.minecraft.util.math.BlockPos;
+
+@Module.Declaration(name = "FastBow", category = Category.Combat)
+public class FastBow extends Module {
+
+    IntegerSetting drawLength = registerInteger("Draw Length", 3, 0, 21);
+
+    public void onUpdate() {
+        if (mc.player.getHeldItemMainhand().getItem() instanceof ItemBow && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= drawLength.getValue() && !ModuleManager.isModuleEnabled("Quiver")) {
+            mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, mc.player.getHorizontalFacing()));
+            mc.player.connection.sendPacket(new CPacketPlayerTryUseItem(mc.player.getActiveHand()));
+            mc.player.stopActiveHand();
+        }
+    }
+}
