860132ec51ede5685cb9c3b25a47a06f462189f9
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-04-03 21:11:39 -0600

    Update CommandManager.java

--- src/main/java/me/srgantmoomoo/postman/framework/command/CommandManager.java
+++ src/main/java/me/srgantmoomoo/postman/framework/command/CommandManager.java
@@ -23,7 +23,7 @@ import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 
 public class CommandManager {
 	
-	public List<Command> commands = new ArrayList<Command>();
+	public final List<Command> commands = new ArrayList<Command>();
 	public String prefix = ",";
 	
 	public CommandManager() {
@@ -44,68 +44,71 @@ public class CommandManager {
 	
 	@EventHandler
 	public Listener<ClientChatEvent> listener = new Listener<>(event -> {
-		String message = event.getMessage();
+	    var message = event.getMessage();
 
-		if(!message.startsWith(prefix))
-			return;
+	    if(!message.startsWith(prefix))
+		return;
 
-		event.setCanceled(true);
-		message = message.substring(prefix.length());
+	    event.setCanceled(true);
+	    message = message.substring(prefix.length());
 
-		if(message.split(" ").length > 0) {
-			boolean commandFound = false;
-			String commandName = message.split(" ")[0];
-			if(commandName.equals("") || commandName.equals("help")) {
-				ChatFormatting GRAY = ChatFormatting.GRAY;
-				ChatFormatting BOLD = ChatFormatting.BOLD;
-				ChatFormatting RESET = ChatFormatting.RESET;
-				sendClientChatMessage("\n" + GRAY + "" + BOLD + "i love postman <3" + "\n" + RESET, false);
-				sendCommandDescriptions();
-				sendClientChatMessage("\n" + RESET + GRAY + BOLD + "i hate postman." + "\n", false);
-			} else {
-				for (Command c : commands) {
-					if (c.aliases.contains(commandName) || c.name.equalsIgnoreCase(commandName)) {
-						c.onCommand(Arrays.copyOfRange(message.split(" "), 1, message.split(" ").length), message);
-						commandFound = true;
-						break;
-					}
-				}
-				if (!commandFound) {
-					sendClientChatMessage(ChatFormatting.DARK_RED + "command does not exist, use " + ChatFormatting.ITALIC + prefix + "help " + ChatFormatting.RESET + "" + ChatFormatting.DARK_RED + "for help.", true);
-				}
+	    if(message.split(" ").length > 0) {
+		boolean commandFound = false;
+		String commandName = message.split(" ")[0];
+		    
+		if(commandName.equals("") || commandName.equals("help")) {
+		    ChatFormatting GRAY = ChatFormatting.GRAY;
+		    ChatFormatting BOLD = ChatFormatting.BOLD;
+		    ChatFormatting RESET = ChatFormatting.RESET;
+		    sendClientChatMessage("\n" + GRAY + "" + BOLD + "i love postman <3" + "\n" + RESET, false);
+		    sendCommandDescriptions();
+		    sendClientChatMessage("\n" + RESET + GRAY + BOLD + "i hate postman." + "\n", false);
+		} 
+		else {
+		    for (Command c : commands) {
+			if (c.aliases.contains(commandName) || c.name.equalsIgnoreCase(commandName)) {
+			    c.onCommand(Arrays.copyOfRange(message.split(" "), 1, message.split(" ").length), message);
+			    commandFound = true;
+			    break;
 			}
+		    }
+		    if (!commandFound) {
+			sendClientChatMessage(ChatFormatting.DARK_RED + "command does not exist, use " + ChatFormatting.ITALIC + prefix + "help " + ChatFormatting.RESET + "" + ChatFormatting.DARK_RED + "for help.", true);
+		    }
 		}
-	    });
+	    }
+	});
 
 	//TODO find a better color for syntax or something lol.
 	private void sendCommandDescriptions() {
-		ChatFormatting GRAY = ChatFormatting.GRAY;
-		ChatFormatting RED = ChatFormatting.RED;
-		ChatFormatting ITALIC = ChatFormatting.ITALIC;
-		for(Command c : Main.INSTANCE.commandManager.commands) {
-			sendClientChatMessage(c.name + " - " + GRAY + c.description + RED + ITALIC + " [" + c.syntax + "]", false);
-		}
+	    ChatFormatting GRAY = ChatFormatting.GRAY;
+	    ChatFormatting RED = ChatFormatting.RED;
+	    ChatFormatting ITALIC = ChatFormatting.ITALIC;
+		
+	    for(Command c : Main.INSTANCE.commandManager.commands) {
+		sendClientChatMessage(c.name + " - " + GRAY + c.description + RED + ITALIC + " [" + c.syntax + "]", false);
+	    }
 	}
 	
 	@SubscribeEvent
 	public void openChatScreen(KeyInputEvent e) {
-		if (prefix.length() == 1) {
-            final char key = Keyboard.getEventCharacter();
-            if (prefix.charAt(0) == key) {
-                Minecraft.getMinecraft().displayGuiScreen(new GuiChat());
-				assert Minecraft.getMinecraft().currentScreen != null;
-				((GuiChat) Minecraft.getMinecraft().currentScreen).inputField.setText(prefix);
-            }
-        }
+	    if (prefix.length() == 1) {
+	        final char key = Keyboard.getEventCharacter();
+	        if (prefix.charAt(0) == key) {
+		    Minecraft.getMinecraft().displayGuiScreen(new GuiChat());
+		    assert Minecraft.getMinecraft().currentScreen != null;
+		    ((GuiChat) Minecraft.getMinecraft().currentScreen).inputField.setText(prefix);
+	    	}
+	    }
 	}
 	
 	public void setCommandPrefix(String pre) {
-        prefix = pre;
+            prefix = pre;
         
-        if(Main.INSTANCE.saveLoad != null) {
-			Main.INSTANCE.saveLoad.save();
-		}
-    }
+	    if(Main.INSTANCE.saveLoad != null) {
+		Main.INSTANCE.saveLoad.save();
+	    }
+	}
 
 	public void sendClientChatMessage(String message, boolean prefix) {
 		String messageWithPrefix = ChatFormatting.GRAY + "@" + ChatFormatting.ITALIC + Reference.NAME + ChatFormatting.RESET + ": " + message;
