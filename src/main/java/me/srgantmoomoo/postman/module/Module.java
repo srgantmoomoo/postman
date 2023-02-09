67b1812d055099eec6e39f6c2ea42f648804fc52
srgantmoomoo <69589624+srgantmoomoo@users.noreply.github.com>
2023-02-08 23:32:58 -0500

    start of setting stuff but im too lazy to fix it <3.

--- src/main/java/me/srgantmoomoo/postman/module/Module.java
+++ src/main/java/me/srgantmoomoo/postman/module/Module.java
@@ -1,11 +1,17 @@
 package me.srgantmoomoo.postman.module;
 
+import me.srgantmoomoo.postman.module.setting.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Module {
 
     private final String name;
     private final String description;
     private final Category category;
     private boolean enabled;
+    private List<Setting> settings = new ArrayList<>();
 
     public Module(String name, String description, Category category) {
         this.name = name;
---
+++ src/main/java/me/srgantmoomoo/postman/module/setting/Setting.java
@@ -0,0 +1,16 @@
+package me.srgantmoomoo.postman.module.setting;
+
+import me.srgantmoomoo.postman.module.Module;
+
+public class Setting {
+    private String name;
+    private Module parent;
+
+    public String getName() {
+        return name;
+    }
+
+    public Module getParent() {
+        return parent;
+    }
+}
