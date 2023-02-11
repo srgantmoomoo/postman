16b952cbcb4ee2a79f1c6897b031298c7d7490aa
srgantmoomoo <69589624+srgantmoomoo@users.noreply.github.com>
2023-02-10 19:41:55 -0500

    im not intelligent.

--- src/main/java/me/srgantmoomoo/postman/mixins/MixinKeyboard.java
+++ src/main/java/me/srgantmoomoo/postman/mixins/MixinKeyboard.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Keyboard.class)
 public class MixinKeyboard {
-    @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "onKey", at = @At(value = "INVOKE", target = "net/minecraft/client/util/InputUtil.isKeyPressed(JI)Z", ordinal = 5), cancellable = true)
     public void onKeyPressed(long window, int key, int scanCode, int action, int modifiers, CallbackInfo info) {
         Main.INSTANCE.moduleManager.keyPress(key, scanCode);
     }
