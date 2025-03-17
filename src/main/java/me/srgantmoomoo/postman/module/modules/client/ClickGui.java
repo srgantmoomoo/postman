de5f55e1f54a56aa957e14dc66dcb7256df40e35
srgantmoomoo <69589624+srgantmoomoo@users.noreply.github.com>
2025-03-17 09:21:25 -0400

    closing gui fix.

--- src/main/java/me/srgantmoomoo/postman/module/modules/client/ClickGui.java
+++ src/main/java/me/srgantmoomoo/postman/module/modules/client/ClickGui.java
@@ -2,9 +2,12 @@ package me.srgantmoomoo.postman.module.modules.client;
 
 import me.srgantmoomoo.postman.Main;
 import me.srgantmoomoo.postman.clickgui.ClickGuiScreen;
+import me.srgantmoomoo.postman.event.Event;
+import me.srgantmoomoo.postman.event.events.EventGuiKeyPress;
 import me.srgantmoomoo.postman.module.Category;
 import me.srgantmoomoo.postman.module.Module;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import org.lwjgl.glfw.GLFW;
 
 public class ClickGui extends Module {
@@ -21,5 +24,19 @@ public class ClickGui extends Module {
     @Override
     public void onDisable() {
         //Main.INSTANCE.save.saveGui(); // gui
+    }
+
+    @Override
+    public void onEvent(Event e) {
+        if(e instanceof EventGuiKeyPress) {
+            if(((EventGuiKeyPress) e).getKey() == GLFW.GLFW_KEY_ESCAPE)
+                this.disable();
+            /* something like this
+            if(((EventGuiKeyPress) e)..getKey() == this.getKey()) {
+                //MinecraftClient.getInstance().setScreen(Screen);
+                MinecraftClient.getInstance().player.closeScreen();
+                this.disable();
+            }*/
+        }
     }
 }
