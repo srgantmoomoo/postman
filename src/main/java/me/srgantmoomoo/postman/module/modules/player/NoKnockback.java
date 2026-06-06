d17916b102bb1b8673b07cc19f02143f54d8e2f2
srgantmoomoo <69589624+srgantmoomoo@users.noreply.github.com>
2026-06-05 23:40:23 -0400

    velocity module fix.

--- src/main/java/me/srgantmoomoo/postman/module/modules/player/NoKnockback.java
+++ src/main/java/me/srgantmoomoo/postman/module/modules/player/NoKnockback.java
@@ -8,6 +8,9 @@ import me.srgantmoomoo.postman.module.setting.settings.NumberSetting;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.Optional;
 
 public class NoKnockback extends Module {
     public NumberSetting playerKnockback = new NumberSetting("player", this, 0, 0, 100, 1);
@@ -17,7 +20,7 @@ public class NoKnockback extends Module {
         super("noKnockback", "take no knockback when taking damage.", Category.PLAYER, 0);
         this.addSettings(playerKnockback, explosionKnockback);
     }
-/*
+
     @Override
     public void onEvent(Event e) {
         if(MinecraftClient.getInstance().player == null)
@@ -25,7 +28,7 @@ public class NoKnockback extends Module {
 
         if(e instanceof EventPacket.Receive) {
             if(((EventPacket.Receive) e).getPacket() instanceof EntityVelocityUpdateS2CPacket p) {
-                if(p.getId() == MinecraftClient.getInstance().player.getId()) {
+                if(p.getEntityId() == MinecraftClient.getInstance().player.getId()) {
                     double velocity = playerKnockback.getValue() / 100;
                     double velocityX = (p.getVelocityX() / 8000d - MinecraftClient.getInstance().player.getVelocity().x) * velocity;
                     double velocityY = (p.getVelocityY() / 8000d - MinecraftClient.getInstance().player.getVelocity().y) * velocity;
@@ -36,12 +39,16 @@ public class NoKnockback extends Module {
                 }
             }
             if(((EventPacket) e).getPacket() instanceof ExplosionS2CPacket p1) {
-                double velocity = explosionKnockback.getValue() / 100;
-                p1.playerVelocityX = (float) (p1.getPlayerVelocityX() * velocity);
-                p1.playerVelocityY = (float) (p1.getPlayerVelocityY() * velocity);
-                p1.playerVelocityZ = (float) (p1.getPlayerVelocityZ() * velocity);
+                p1.playerKnockback().ifPresent(knockback -> {
+                    double velocity = explosionKnockback.getValue() / 100;
+                    p1.playerKnockback = Optional.of(new Vec3d(
+                            knockback.x * velocity,
+                            knockback.y * velocity,
+                            knockback.z * velocity
+                    ));
+                });
             }
         }
-    }*/
+    }
 
 }
